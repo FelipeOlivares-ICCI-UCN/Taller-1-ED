26ba71fff39591435112e79f013dedbc43c4ceaa
Felipe Olivares <felipe.olivares04@alumnos.ucn.cl>
2026-09-14 13:20:14 -0300

    fix: ortography

--- src/objetos/Pacientes.cpp
+++ src/objetos/Pacientes.cpp
@@ -10,16 +10,21 @@ Paciente::Paciente(string ID, string Nombre, string edad, string Servicio) {
 string Paciente::getServicio() const { return this->Servicio; }
 
 ostream& operator<<(ostream& o, const Paciente& p) {
-    o << p.ID << " - " << p.Nombre;
+    o << p.ID << " - " << p.Nombre << " (" << p.edad << ")";
     return o;
 }
 
 void Paciente::showFullInformation() const {
-    std::cout << "ID: " << this->ID << endl;
-    std::cout << "Nombre: " << this->Nombre << endl;
-    std::cout << "Edad: " << this->edad << endl;
-    std::cout << "Servicio: " << this->Servicio << endl;
+        std::cout << "--------------------" << std::endl;
+        std::cout << "ID: " << this->ID << endl;
+        std::cout << "Nombre: " << this->Nombre << endl;
+        std::cout << "Edad: " << this->edad << endl;
+        std::cout << "Servicio: " << this->Servicio << endl;
 
-}
+    }
+
+
+
+
 
 
