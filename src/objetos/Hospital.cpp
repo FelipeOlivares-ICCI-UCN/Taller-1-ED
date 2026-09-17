0411f4cc7e0edcc8d6059503435dae664f56c55a
Felipe Olivares <felipe.olivares04@alumnos.ucn.cl>
2026-09-17 18:46:40 -0300

    add: added Pacients to records

--- src/objetos/Hospital.cpp
+++ src/objetos/Hospital.cpp
@@ -17,38 +17,48 @@ void Hospital::addPaciente(const Paciente& paciente) {
     std::cout << "Enviando a " << paciente.getServicio() << endl;
 
     if (paciente.getServicio() == "Urgencias") {
-
         Departamentos.get(0).insertLast(paciente);
+        historial.push(paciente);
+
     }
     else if (paciente.getServicio() == "Medicina General") {
         Departamentos.get(1).insertLast(paciente);
+        historial.push(paciente);
     }
     else if (paciente.getServicio() == "Cardiologia") {
         Departamentos.get(2).insertLast(paciente);
+        historial.push(paciente);
     }
     else if (paciente.getServicio() == "Neurologia") {
         Departamentos.get(3).insertLast(paciente);
+        historial.push(paciente);
     }
 
     else if (paciente.getServicio() == "Traumatologia") {
         Departamentos.get(4).insertLast(paciente);
+        historial.push(paciente);
     }
 
     else if (paciente.getServicio() == "Cirugia") {
         Departamentos.get(5).insertLast(paciente);
+        historial.push(paciente);
     }
 
     else if (paciente.getServicio() == "Hospitalizacion") {
         Departamentos.get(6).insertLast(paciente);
+        historial.push(paciente);
     }
 
     else if (paciente.getServicio() == "Pediatria") {
         Departamentos.get(7).insertLast(paciente);
+        historial.push(paciente);
     }
 
     else {
         std::cout << "Rechazado, Servicio desconocido: " << paciente.getServicio()  << std::endl;
     }
+}
+
 void Hospital::showPacientes(int depNum) {
     if (depNum < 1 || depNum > 8) {
         cerr << "That departament number doesnt exist" << endl;
